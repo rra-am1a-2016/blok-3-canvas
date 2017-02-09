7f12e0ab220f91d762400c21d441367c3dc93f2f
Arjan de Ruijter <rra@mboutrecht.nl>
2017-02-09 11:31:53 +0100

    plaatje laten bewegen

---
+++ canvasImage-Text/canvasImage.js
@@ -0,0 +1,44 @@
+// Maak een canvas object
+var canvas = document.getElementById("myCanvas");
+
+// Maake een context object
+var context = canvas.getContext("2d");
+
+// Bepaal de kleur van je Letters
+context.fillStyle = "#121212";
+
+// Bepaal de font
+context.font = "40px Trebuchet MS";
+
+// Teken het op het scherm
+context.fillText("Van bewegen wordt ik blij!", 60, 70);
+
+var image = new Image();
+
+image.src = "sad.png";
+
+var x = 250;
+var y = 150;
+var speedX = 0;
+var speedY = 0;
+
+context.drawImage(image, x, y, image.width/3, image.height/3);
+
+
+setInterval(updateGame, 20);
+
+function updateGame () {
+   context.clearRect(300, 0, 150, 150);
+   newPos();
+   context.fillText("Van bewegen wordt ik blij!", 60, 70);
+   context.drawImage(image, x, y, image.width/3, image.height/3);
+}
+
+function newPos () {
+   x += speedX;
+   y += speedY;
+}
+
+
+
+
