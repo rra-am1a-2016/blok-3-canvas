82dcdfdd295f001ca50c3bc2da3fd277715de9ae
Arjan de Ruijter <rra.am1b.2016@gmail.com>
2017-02-13 13:40:11 +0100

    rectangels

--- staafdiagramAjax/canvas.js
+++ staafdiagramAjax/canvas.js
@@ -1,6 +1,38 @@
-// Maak een canvas element met 25 staafjes erop 
-// Ze moeten verschillen in lengte.
-// Ze moeten verschillen in kleur.
+// Maak een array van javascript objecten
+
+var data = [{ "id": 1,
+              "name": "Arjan",
+              "numberOfContacts": 200 },
+            { "id": 2,
+              "name": "Bert",
+              "numberOfContacts": 10 },
+            { "id": 3,
+              "name": "Hans",
+              "numberOfContacts": 168 },
+            { "id": 4,
+              "name": "Dewi",
+              "numberOfContacts": 230 },
+            { "id": 5,
+              "name": "Gert",
+              "numberOfContacts": 78 },
+            { "id": 6,
+              "name": "Stoffel",
+              "numberOfContacts": 123 },
+            { "id": 7,
+              "name": "Harry",
+              "numberOfContacts": 346 },
+            { "id": 8,
+              "name": "Jan",
+              "numberOfContacts": 189 },
+            { "id": 9,
+              "name": "Bob",
+              "numberOfContacts": 277 },
+            { "id": 10,
+              "name": "James",
+              "numberOfContacts": 300 }];
+
+// De lengte van het array is:
+var arrayLength = data.length;
 
 // Selecteer het canvas element op basis van zijn id.
 var canvas = document.getElementById("myCanvas");
@@ -16,24 +48,17 @@ var colorArray = ["blue", "green", "yellow", "red", "orange", "purple", "grey", 
 
 // Math.floor(), Math.random() *
 function drawCanvas () {
-   for ( var i = 0; i < 14; i++ )
+   for ( var i = 0; i < arrayLength; i++ )
    {
       // Geef het te tekenen object een kleur
       context.fillStyle = randomColor(colorArray);
 
       // Teken een rechthoek met het context-object.
-      context.fillRect(10 + i * 20, 145, 6, -140 + i * 10);
-   }
-   
+      context.fillRect(40 + i * 30, 360, 12, -1 * data[i].numberOfContacts);
+   }   
 }
 
-// clearInterval()   
-
-var interval = window.setInterval(drawCanvas, 500);
-
-document.getElementById("btn").onclick = function () {
-   window.clearInterval(interval);
-}
+drawCanvas();
 
 function randomColor (colorArray) {
    var index = Math.floor(Math.random() * colorArray.length);
